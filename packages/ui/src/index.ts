6963ae713d08be1893bfec4a74dbf8b7d3bfdb5d
renaud <renaud1.juliani@epitech.eu>
2026-04-02 20:57:58 +0200

    feat(ui): scaffold packages/ui

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,12 @@
+// Stores
+export { createCharacterStore } from "./stores/characterStore.js";
+
+// Hooks
+export { useQuestToggle } from "./hooks/useQuestToggle.js";
+export { useResources } from "./hooks/useResources.js";
+
+// Constants
+export {
+  QUEST_TYPE_BADGE_CONFIG,
+  DOFUS_CLASSES,
+} from "./constants/dofusColors.js";
