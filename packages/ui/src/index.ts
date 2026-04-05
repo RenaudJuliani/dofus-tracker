5c71509492ea8c22d49b2cefc70466357c527fb6
renaud <renaud1.juliani@epitech.eu>
2026-04-05 10:37:08 +0200

    fix(ui): remove .js extensions from barrel exports for webpack compatibility

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,13 +1,13 @@
 // Stores
-export { createCharacterStore } from "./stores/characterStore.js";
-export type { CharacterStore } from "./stores/characterStore.js";
+export { createCharacterStore } from "./stores/characterStore";
+export type { CharacterStore } from "./stores/characterStore";
 
 // Hooks
-export { useQuestToggle } from "./hooks/useQuestToggle.js";
-export { useResources } from "./hooks/useResources.js";
+export { useQuestToggle } from "./hooks/useQuestToggle";
+export { useResources } from "./hooks/useResources";
 
 // Constants
 export {
   QUEST_TYPE_BADGE_CONFIG,
   DOFUS_CLASSES,
-} from "./constants/dofusColors.js";
+} from "./constants/dofusColors";
